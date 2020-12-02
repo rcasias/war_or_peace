b2d8993a9412f712ce146abe52b97bab58bab215
Regina Casias <reginaannvigil@gmail.com>
2020-12-01 19:29:17 -0700

    "inital commit"

---
+++ lib/deck.rb
@@ -0,0 +1,11 @@
+class Deck
+  attr_reader :cards
+
+  def initialize()
+    @cards = []
+  end
+
+  def add_card_to_deck(card)
+    @cards << card
+  end
+end
--- test/card_test.rb
+++ test/card_test.rb
@@ -1,6 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/card'
+require '../lib/card'
+
+jack = Card.new(:heart, "Jack", 11)
+puts jack.suit
+require 'pry'; binding.pry
 
 class CardTest < Minitest::Test
   def setup
@@ -15,7 +19,7 @@ class CardTest < Minitest::Test
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
+
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
