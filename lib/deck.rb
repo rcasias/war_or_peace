0e1eec7e1e8829ee92b41e1ed23165d1982d526b
Regina Casias <reginaannvigil@gmail.com>
2020-12-02 23:16:55 -0700

    commit changes

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,27 +2,40 @@
 class Deck
   attr_reader :cards
 
-  def initialize()
-    @cards = []
+  def initialize(cards)
+    @cards = cards
   end
 
-  def add_card_to_deck(card)
-    @cards << card
-  end
-
-  def rank_of_cards_at()
+  def rank_of_card_at(index)
+    return @cards[index].rank
   end
 
   def high_ranking_cards()
+    high_card = []
+    @cards.each do |card|
+      if card.rank >= 11
+        high_card << card
+      end
+    end
+    return high_card
   end
 
-  def percent_high_ranking_cards()
+  def percent_high_ranking()
+    # length of array (@cards) divided by the number of high ranking cards
+    array_length = @cards.length.to_f
+    high_ranking_cards_length = high_ranking_cards.length.to_f
+
+    return (high_ranking_cards_length / array_length).round(4) * 100
   end
 
   def remove_card()
+    # removes the top card of the Deck
+    # The 'Top' card is the first index of the cards array (@cards[0])
+    return @cards.shift
   end
 
-  def add_card()
+  def add_card(card)
+    @cards << card
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,3 +1,43 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/deck'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+  def setup
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    return cards
+  end
+
+  def test_it_exists
+    cards = setup()
+
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_cards_type
+    cards = setup()
+
+    deck = Deck.new(cards)
+
+    deck.cards.each do | card |
+      assert_instance_of Card, card
+    end
+  end
+
+  def test_rank_of_card
+    cards = setup()
+
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+  end
+
+end
