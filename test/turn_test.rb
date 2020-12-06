75c63fe53e6d236ba969178cecc5119bfe8f53e7
Regina Casias <reginaannvigil@gmail.com>
2020-12-06 16:31:54 -0700

    added turn test tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,48 +5,123 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
+class TurnTest < Minitest::Test
+  def test_turn_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    cards1 = [card1, card3, card5]
+    cards2 = [card2, card4, card6]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new("Clarisa", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_instance_of Turn, turn
+  end
+  #
+  def test_two_players_exist
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    cards1 = [card1, card3, card5]
+    cards2 = [card2, card4, card6]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_instance_of Player, turn.player1
+    assert_instance_of Player, turn.player2
+  end
+  #
+  #
+  def test_spoils_of_war
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    cards1 = [card1, card3, card5]
+    cards2 = [card2, card4, card6]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new("Clarisa", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal [], turn.spoils_of_war
+  end
 
-def turn_test_exists
-  turn = Turn.new(player1, player2)
-  player1 = Player.new("Clarisa", deck1)
-  player2 = Player.new("Aurora", deck2)
-  deck = Deck.new(cards)
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
+  def test_type_basic
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    cards1 = [card1, card3, card5]
+    cards2 = [card2, card4, card6]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new("Clarisa", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal :basic, turn.type
+  end
+  def test_type_war
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, 'Queen', 12)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    cards1 = [card1, card3, card5]
+    cards2 = [card2, card4, card6]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new("Clarisa", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal :war, turn.type
+  end
+  def test_type_mutually_assured_destruction
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, 'Queen', 12)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:diamond, '8', 8)
+    card5 = Card.new(:heart, 'Jack', 11)
+    card6 = Card.new(:diamond, 'Jack', 11)
+    cards1 = [card1, card3, card5]
+    cards2 = [card2, card4, card6]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new("Clarisa", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal :mutually_assured_destruction, turn.type
+  end
 
-  cards = [card1, card2, card3]
-  assert_instance_of Turn, turn
-end
-
-def test_two_players_exist
-  player1 = Player.new("Clarisa", deck1)
-  player2 = Player.new("Aurora", deck2)
-
-  assert_equal player1, turn.player1
-  assert_equal player2, turn.player2
-end
-
-
-def spoils_of_war
-  turn = turn.new(player1, player2)
-  player1 = Player.new("Clarisa", deck1)
-  player2 = Player.new("Aurora", deck2)
-  assert_equal [], turn.spoils_of_war
-end
-
-def test_type
-  turn = Turn.new(player1, player2)
-  player1 = Player.new("Clarisa", deck1)
-  player2 = Player.new("Aurora", deck2)
-  deck = Deck.new(cards)
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  card4 = Card.new(:diamond, 'Jack', 11)
-  card5 = Card.new(:heart, '8', 8)
-  card6 = Card.new(:diamond, 'Queen', 12)
-  assert_equal :basic, turn.basic
-  assert_equal :war, turn.war
-  assert_equal :mutually_assured_destruction, turn.mutually_assured_destruction
+  def test_winner
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:diamond, '8', 8)
+    card5 = Card.new(:heart, 'Jack', 11)
+    card6 = Card.new(:diamond, 'Jack', 11)
+    cards1 = [card1, card3, card5]
+    cards2 = [card2, card4, card6]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal "Megan", turn.winner.name
+  end
 end
